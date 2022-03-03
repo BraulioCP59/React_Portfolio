5cef2ffea6b8d8a1adb9af2f54be126e3a49f013
Braulio Mora <brauliocdc59@gmail.com>
2022-03-02 23:24:14 -0800

    updated the contact form with handler to flag required fields on onblur with empty text

--- react-shop/README.md
+++ react-shop/README.md
@@ -20,27 +20,43 @@ SO THAT I can assess whether they're a good candidate for an open position
 
 ## Acceptance Criteria
 
-```md
-GIVEN a single-page application portfolio for a web developer
-WHEN I load the portfolio
-THEN I am presented with a page containing a header, a section for content, and a footer
-
-WHEN I am presented with the Portfolio section 
-THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
-
-WHEN I move my cursor out of one of the form fields without entering text 
-THEN I receive a notification that this field is required
-
 WHEN I enter text into the email address field
 THEN I receive a notification if I have entered an invalid email address
 
 WHEN I am presented with the Resume section 
 THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
 
-WHEN I view the footer
-THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
+WHEN I am presented with the Portfolio section
+THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
+
 
 --------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+WHEN I move my cursor out of one of the form fields without entering text  - Done
+THEN I receive a notification that this field is required - Done
+
+WHEN I load the portfolio - Done
+THEN I am presented with a page containing a header, a section for content, and a footer - Done
+
+WHEN I view the footer - Done
+THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) - Done
+
 WHEN I view the header - Done
 THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio - Done
 
--- react-shop/src/components/pages/Contact/Contact.js
+++ react-shop/src/components/pages/Contact/Contact.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Contact()
 {
+    const alertOutline = 'appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white';
+    const outline = 'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500';
+    const messageText = 'no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none';
+    const messageTextAlert = 'no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none'
+    const alertMessage = <p className="text-red-500 text-xs italic">Please fill out this field.</p>;
+
+    const [alert, setAlert] = useState(false);
+    const [alertTextMessage, setAlertTextMessage] = useState(false);
+
+    const handleRequiredFields = (e) =>{
+        if(e.target.value)
+        {
+            setAlert(false);
+            setAlertTextMessage(false);
+        }else
+        {
+            setAlert(true);
+            setAlertTextMessage(true);
+        }
+   }
     return(
         <div className='flex justify-center'>
             <div>
@@ -11,14 +31,15 @@ function Contact()
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                             First Name
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
-                        <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                        <input onBlur={(e) => handleRequiredFields(e)} className={alert? alertOutline: outline} id="grid-first-name" type="text" placeholder="Jane"/>
+                        {alertTextMessage? alertMessage: <br/>}
                         </div>
                         <div className="w-full md:w-1/2 px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                             Last Name
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
+                        <input onBlur={(e) => handleRequiredFields(e)} className={alert? alertOutline: outline} id="grid-last-name" type="text" placeholder="Doe"/>
+                        {alertTextMessage? alertMessage: <br/>}
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
@@ -26,7 +47,7 @@ function Contact()
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                             E-mail
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email"/>
+                        <input onBlur={(e) => handleRequiredFields(e)} className={alert? alertOutline: outline} id="email" type="email"/>
                         <p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
                         </div>
                     </div>
@@ -35,7 +56,7 @@ function Contact()
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                             Message
                         </label>
-                        <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+                        <textarea onBlur={(e) => handleRequiredFields(e)} className={alert? messageTextAlert: messageText} id="message"></textarea>
                         <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p>
                         </div>
                     </div>
